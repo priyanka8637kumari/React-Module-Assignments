2b7277e6b80a4109ff746e13f0ce7f1d37e4257f
Priyanka <priyanka.kumari@hyperisland.se>
2025-01-17 00:46:00 +0530

    Assignment 7 completed

--- 7/script.js
+++ 7/script.js
@@ -7,7 +7,16 @@
  *   reverseString("world") should return "dlrow"
  *   reverseString("12345") should return "54321"
  */
-function reverseString() {}
+function reverseString(str) {
+  let reverseStr= "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+}
+console.log(reverseString("hello"));
+console.log(reverseString("world"));
+console.log(reverseString("12345"));
 
 /**
  * TODO write a JavaScript function that takes a number and returns it in the reverse order.
@@ -18,7 +27,17 @@ function reverseString() {}
  *   reverseNumber(101010) should return 10101
  */
 
-function reverseNumber() {}
+function reverseNumber(num) {
+  let reverseNum = 0;
+  while (num > 0) {
+    reverseNum = reverseNum * 10 + (num % 10);
+    num = Math.floor(num / 10);
+  }
+  return reverseNum;
+}
+console.log(reverseNumber(12345));
+console.log(reverseNumber(98765));
+console.log(reverseNumber(101010));
 
 /**
  * TODO destruct the following object to get name and age.
@@ -32,11 +51,21 @@ const person = {
   },
 };
 
+const { name, info: { age } } = person; //destructing the above object
+console.log(name);
+console.log(age);
+
+
 /**
  * TODO destruct the following array the first and second element, and the remaining elements in a third variable.
  */
 
 const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
+
+const [first, second, ...remaining] = thisArray; //destructing the above array
+console.log(first);
+console.log(second);
+console.log(remaining);
 
 /**
  * !!!!OPTIONAL!!!!
@@ -50,4 +79,24 @@ const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
  *    isPalindrome("coding") should return false
  */
 
-function isPalindrome() {}
+function isPalindrome(str) {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+  let left = 0;
+  let right = cleanedStr.length - 1;
+  
+  while (left < right) {
+      if (cleanedStr[left] !== cleanedStr[right]) {
+          return false; 
+      }
+      left++;
+      right--;
+  }
+  
+  return true; 
+}
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("hello"));
+console.log(isPalindrome("12321"));
+console.log(isPalindrome("race car"));
+console.log(isPalindrome("coding"));
